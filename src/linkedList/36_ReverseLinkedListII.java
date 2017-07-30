5c2d131591d918945fbdc441b6925e907279e42d
y.ping <yh2169@gmail.com>
2017-07-29 22:47:48 -0400

    Create 36_ReverseLinkedListII.java

---
+++ src/linkedList/36_ReverseLinkedListII.java
@@ -0,0 +1,51 @@
+
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param ListNode head is the head of the linked list 
+     * @oaram m and n
+     * @return: The head of the reversed ListNode
+     */
+    public ListNode reverseBetween(ListNode head, int m , int n) {
+        if (n <= m || head == null){
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        for (int i = 1; i < m; i ++){
+            if (head == null){
+                return null;
+            }
+            head = head.next;
+        }
+        
+        ListNode preM = head;
+        ListNode mNode = head.next;
+        ListNode mNodeNext = mNode.next;
+        
+        ListNode prev = null;
+        ListNode curr = mNode;
+        for (int i = m; i <= n; i ++){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        
+        mNode.next = curr;
+        preM.next = prev;
+        return dummy.next;
+        
+    }
+}
