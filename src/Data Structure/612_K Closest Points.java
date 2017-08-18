1be1cf33d497fab79a894ff9349e82e7cc4e0d48
y.ping <yh2169@gmail.com>
2017-08-18 08:24:32 -0400

    Create 612_K Closest Points.java

---
+++ src/Data Structure/612_K Closest Points.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a point.
+ * class Point {
+ *     int x;
+ *     int y;
+ *     Point() { x = 0; y = 0; }
+ *     Point(int a, int b) { x = a; y = b; }
+ * }
+ */
+public class Solution {
+    /**
+     * @param points a list of points
+     * @param origin a point
+     * @param k an integer
+     * @return the k closest points
+     */
+    private Point globalOrigin = null;
+
+    public Point[] kClosest(Point[] points, Point origin, int k) {
+        globalOrigin = origin;
+        PriorityQueue<Point> pq = new PriorityQueue<Point> (k, new Comparator<Point> () {
+            @Override
+            public int compare(Point a, Point b) {
+                int diff = getDistance(b, globalOrigin) - getDistance(a, globalOrigin);
+                if (diff == 0)
+                    diff = b.x - a.x;
+                if (diff == 0)
+                    diff = b.y - a.y;
+                return diff;
+            }
+        });
+
+        for (int i = 0; i < points.length; i++) {
+            pq.offer(points[i]);
+            if (pq.size() > k)
+                pq.poll();
+        }
+        
+        k = pq.size();
+        Point[] ret = new Point[k];  
+        while (!pq.isEmpty())
+            ret[--k] = pq.poll();
+        return ret;
+    }
+    
+    private int getDistance(Point p, Point o){
+        return (p.y- o.y) * (p.y- o.y) + (p.x- o.x) * (p.x- o.x);
+    }
+}
