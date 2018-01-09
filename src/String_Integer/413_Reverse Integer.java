3255c684325aedd32477c1b2d2fba3efb80c2421
y.ping <yh2169@gmail.com>
2018-01-09 14:32:38 -0500

    Create 413_Reverse Integer.java

---
+++ src/String_Integer/413_Reverse Integer.java
@@ -0,0 +1,22 @@
+public class Solution {
+    /*
+     * @param n: the integer to be reversed
+     * @return: the reversed integer
+     */
+    public int reverseInteger(int n) {
+        long result = 0;
+        
+        
+        while (n != 0){
+            long x = result * 10 + n % 10;
+            n /= 10;
+            result = x;
+        }
+        
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE ){
+            return 0;
+        }
+        
+        return (int)result;
+    }
+}
