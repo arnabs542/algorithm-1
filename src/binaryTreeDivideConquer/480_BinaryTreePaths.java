0d923a94259d359f540310e6530a9095c83234d4
y.ping <yh2169@gmail.com>
2018-01-10 17:40:42 -0500

    Update 480_BinaryTreePaths.java

--- src/binaryTreeDivideConquer/480_BinaryTreePaths.java
+++ src/binaryTreeDivideConquer/480_BinaryTreePaths.java
@@ -30,5 +30,54 @@ public class 480_BinaryTreePaths {
 
         return path;
     }
+    
+    
+    //////////////////////
+    //// Version 2    ////
+    //////////////////////
+    
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<String>();
+        List<Integer> path = new ArrayList<Integer>();
+
+        if (root == null) {
+            return result;
+        }
+        
+        path.add(root.val);
+        getPath(root, result, path);
+        
+        return result;
+    }
+    
+    private void getPath(TreeNode node
+                        , List<String> result
+                        , List<Integer> path){
+        if (node == null){
+            return;
+        }
+        
+        if (node.left == null && node.right == null){
+            String pathS = path.get(0).toString();
+            for (int i = 1; i < path.size(); i++){
+                pathS = pathS + "->" + path.get(i).toString();
+            }
+            result.add(pathS);
+        }
+        
+        if (node.left != null){
+            path.add(node.left.val);
+            getPath(node.left, result, path);
+            path.remove(path.size()-1);
+        }
+        
+         if (node.right != null){
+            path.add(node.right.val);
+            getPath(node.right, result, path);
+            path.remove(path.size()-1);
+        }
+        
+    }
+    
 
 }
