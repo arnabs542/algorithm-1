ef4bae245c7f7abea05bc867005ec33c898167f6
y.ping <yh2169@gmail.com>
2018-02-07 07:22:56 -0500

    Update LinkedListOperation.java

--- src/linkedList/LinkedListOperation.java
+++ src/linkedList/LinkedListOperation.java
@@ -44,7 +44,8 @@ public class LinkedListOperation
     // ListNode result = GetMiddleNode(l1);
     // System.out.println((result == null? -99: result.val));	
 	  
-    // 6. Has Cycle
+    // 6. Has Cycle 
+    // 102. Linked List Cycle
     // (2 pointers meet => has cycle)
     // boolean result = hasCycle(l1);
     // System.out.println(result);
