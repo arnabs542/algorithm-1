da362cbb2e5b2e1c957b11149ba4a34b12c5df65
y.ping <yh2169@gmail.com>
2018-03-25 00:01:21 -0400

    Update 585_MaximumNumberinMountaInSequence.java

--- src/Binary Search/585_MaximumNumberinMountaInSequence.java
+++ src/Binary Search/585_MaximumNumberinMountaInSequence.java
@@ -30,3 +30,37 @@ public class Solution {
 
     }
 }
+
+
+public class Solution {
+    /**
+     * @param nums: a mountain sequence which increase firstly and then decrease
+     * @return: then mountain top
+     */
+    public int mountainSequence(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return -1;
+        }
+        
+        int start = 0;
+        int end = nums.length -1;
+        while (start + 1 < end){
+            int mid = (start + end)/2;
+            if (mid - 1 >= 0){
+                if (nums[mid] >= nums[mid - 1]){
+                    start = mid;
+                }else{
+                    end = mid;
+                }
+            } else{
+                start = mid;
+            }
+        }
+        
+        if (nums[start] > nums[end]){
+            return nums[start];
+        }
+        
+        return nums[end];
+    }
+}
