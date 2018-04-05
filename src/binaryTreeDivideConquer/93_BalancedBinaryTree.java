36a5bf1d3be82ea2ba12f49e6e9ffd295bd22ee5
y.ping <yh2169@gmail.com>
2018-04-05 07:59:18 -0400

    Update 93_BalancedBinaryTree.java

--- src/binaryTreeDivideConquer/93_BalancedBinaryTree.java
+++ src/binaryTreeDivideConquer/93_BalancedBinaryTree.java
@@ -4,6 +4,10 @@ package binaryTreeDivideConquer;
  * Created by epingho on 2017/7/4.
  */
 
+///////////////////////////
+//// Solution 1
+///////////////////////////
+
 class ResultType{
     public boolean isBalanced;
     public int maxDepth;
@@ -39,5 +43,46 @@ public class BalancedBinaryTree {
         int depth = Math.max(resultL.maxDepth, resultR.maxDepth) + 1;
         return new ResultType(true, depth);
 
+    }
+}
+
+
+///////////////////////////
+//// Solution 2
+///////////////////////////
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if this Binary tree is Balanced, or false.
+     */
+    public boolean isBalanced(TreeNode root) {
+        return (depth(root) != -1);
+    }
+    
+    private int depth(TreeNode root){
+        if (root == null){
+            return 0;
+        }
+        
+        int leftDepth = depth(root.left);
+        int rightDepth = depth(root.right);
+        
+        if (leftDepth == -1 || rightDepth == -1 || Math.abs(leftDepth - rightDepth) > 1){
+            return -1;
+        } else {
+            return Math.max(leftDepth, rightDepth) + 1;
+        }
     }
 }
