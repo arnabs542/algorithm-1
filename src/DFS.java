57a9ace6535e589e53420aea79d6dea1987d8354
y.ping <yh2169@gmail.com>
2018-04-04 23:25:23 -0400

    Update DFS.java

--- src/DFS.java
+++ src/DFS.java
@@ -1,1 +1,13 @@
+/////////////////////////////
+// inorder travesal
+/////////////////////////////
 
+public void inoder(TreeNode root, List<Integer> result){
+  if (root == null){
+    return;
+  }
+  
+  inorder(root.left, result);
+  result.add(root);
+  inorder(root.right, result);
+}
