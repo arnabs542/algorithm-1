6a71b768cbae333fa9a093f8085f0f68bcc93d39
y.ping <yh2169@gmail.com>
2018-04-08 01:18:28 -0400

    Update 153_Combination Sum II.java

--- src/Depth First Search/153_Combination Sum II.java
+++ src/Depth First Search/153_Combination Sum II.java
@@ -1,69 +1,45 @@
 public class Solution {
-    /*
+    /**
      * @param num: Given the candidate numbers
      * @param target: Given the target number
      * @return: All the combinations that sum to target
      */
     public List<List<Integer>> combinationSum2(int[] num, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        if (num == null || num.length == 0) {
+        if (num == null){
             return result;
         }
         
         Arrays.sort(num);
-        //int[] candidates = removeDuplicates(num);
-
-        dfs(num, target, new ArrayList<Integer>(), result, target, 0);
-        
-        return result;    
-        
+        dfs(num, target, 0, new ArrayList<Integer>(), result);
+        return result;
     }
     
-    private int[] removeDuplicates(int[] num){
-        
-        int index = 0;
-        for (int i = 0; i < num.length -1; i++){
-            if (num[i] != num[i+1]){
-                num[index] = num[i];
-                index++;
-            }
-        }
-        
-        int[] candidates = new int[index];
-        for (int j = 0; j < index; j ++){
-            candidates[j] = num[j];
-        }
-        
-        return candidates;
-    }
+    //1, 1, 2, 5, 6, 7, 10
     
-    private void dfs(
-        int[] nums 
-        , int target
-        , List<Integer> temp 
-        , List<List<Integer>> result
-        , int remaining
-        , int startIndex
-      ){
-    
-        if (remaining == 0){
-            if (result.contains(temp)){
+    private void dfs(int[] nums
+                , int target
+                , int startIndex
+                , List<Integer> subset
+                , List<List<Integer>> result
+        ){
+            if (target == 0){
+                result.add(new ArrayList<Integer>(subset));
+                System.out.println("resut add: " + subset);
                 return;
             }
-        	result.add(new ArrayList<Integer>(temp));
-          	return;
-        }
-    
-        for (int i = startIndex; i < nums.length; i ++){
-    
-        if (nums[i] > remaining){
-            break;
-        }
-      
-        temp.add(nums[i]);
-        dfs(nums, target, temp, result, remaining - nums[i], i+1);
-        temp.remove(temp.size()-1);
             
-        }
-    } 
+            for (int i = startIndex; i < nums.length; i ++){
+                if (i != 0 && i != startIndex && nums[i] == nums[i - 1]){
+                    continue;
+                }
+                if (nums[i] > target){
+                    break;
+                }
+                subset.add(nums[i]);
+                dfs(nums, target - nums[i], i + 1, subset, result);
+                subset.remove(subset.size() - 1);
+            }
+        
+    }
 }
