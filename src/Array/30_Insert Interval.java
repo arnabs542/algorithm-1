14a414188011819447275ec9ba3ca433cccd14e4
y.ping <yh2169@gmail.com>
2018-04-19 00:42:32 -0400

    Update 30_Insert Interval.java

--- src/Array/30_Insert Interval.java
+++ src/Array/30_Insert Interval.java
@@ -35,3 +35,61 @@ public class Solution {
         return result;
     }
 }
+
+
+
+public class Solution {
+    /**
+     * @param intervals: Sorted interval list.
+     * @param newInterval: new interval.
+     * @return: A new interval list.
+     */
+    private Interval last = null;
+
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        if (intervals.size() == 0){
+            intervals.add(0, newInterval);
+            return intervals;
+        }
+        
+        List<Interval> result = new ArrayList<Interval>();
+        int index = 0;
+        boolean inserted = false;
+        Interval curr = null;
+        while ( index < intervals.size() && !inserted){
+            if (intervals.get(index).start < newInterval.start){
+                curr = intervals.get(index);
+                index ++;
+            }else{
+                curr = newInterval;
+                inserted = true;
+            }
+            
+            mergeResult(curr, result);
+        }
+        
+        while (index < intervals.size()){
+            curr = intervals.get(index);
+            mergeResult(curr, result);
+            index ++;
+        }
+        
+        if (!inserted){
+            curr = newInterval;
+            mergeResult(curr, result);
+        }
+        
+        return result;
+    }
+    
+    private void mergeResult(Interval curr, List<Interval> result){
+            if (last == null || last.end < curr.start){
+                last = curr;
+                result.add(last);
+            }else{
+                last.start = Math.min(curr.start, last.start);
+                last.end = Math.max(curr.end, last.end);
+            }
+        }
+        
+}
