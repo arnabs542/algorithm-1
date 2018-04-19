4cac1c31e4416127d1fd0569df0afab91376ec7b
y.ping <yh2169@gmail.com>
2018-04-19 07:43:05 -0400

    Update intervals.java

--- src/Array/intervals.java
+++ src/Array/intervals.java
@@ -1,21 +1,25 @@
-// 從start小的開始看
-
+// 這類問題 要從start小的開始看
 // insert intervals
+// 156. Merge Intervals 
+// 839. Merge Two Sorted Interval Lists 
+// 486. Merge K Sorted Arrays 
+
 
 // merge intervals
-// 156. Merge Intervals 
 
 // merge 2 sorted interval list
 // time: O(m + n)
 // 1. Interval curr = null, last = null;
 // 2. 比start, 看要從哪一個拿出來, 從小的開始拿, 設為curr
 // 3. 拿出來後, 跟reuslt比, 沒交集插入, 設為last, 有交集改last
-// 839. Merge Two Sorted Interval Lists 
+// 4. 改交集:
+// last.start = Math.min(last.start, curr.start);
+// last.end = Math.max(last.end, curr.end);
+
 
 // merge k sorted interval list
 // time: O(nklog(k))
 // space: O(k)
-// 486. Merge K Sorted Arrays 
 // 要開一個class存現在是做到哪一個interval
 public class IntervalLocation{
         int row;
