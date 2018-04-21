e6b1f14eb7f0c80f5cb2b82b51f5a0da03afb76e
y.ping <yh2169@gmail.com>
2018-04-21 00:41:04 -0400

    Update 61_Search for a Range.java

--- src/Binary Search/61_Search for a Range.java
+++ src/Binary Search/61_Search for a Range.java
@@ -1,3 +1,85 @@
+//// 基本款
+
+public class Solution {
+    /**
+     * @param A: an integer sorted array
+     * @param target: an integer to be inserted
+     * @return: a list of length 2, [index1, index2]
+     */
+    public int[] searchRange(int[] A, int target) {
+        int[] result = {-1, -1};
+
+        if (A == null || A.length == 0){
+            return result;
+        }
+        
+        int targetIndex = findTarget(A, target);
+        int upperBound, lowerBound;
+        
+        if (targetIndex == -1){
+            return result;
+        }else{
+            upperBound = targetIndex;
+            while(upperBound < A.length - 1 && A[upperBound + 1] == target){
+                upperBound ++;
+            }
+            result[1] = upperBound;
+            
+            lowerBound = targetIndex;
+            while(lowerBound > 0 && A[lowerBound - 1] == target){
+                lowerBound --;
+            }
+            result[0] = lowerBound;
+        }
+
+        return result;
+    }
+    
+    private int findTarget(int[] A, int target){
+        int start = 0;
+        int end = A.length - 1;
+        while (start + 1 < end){
+            int mid = (start + end)/2;
+            if (A[mid] == target){
+                return mid;
+            }else if (A[mid] > target){
+                end = mid;
+            }else{
+                start = mid;
+            }
+        }
+        
+        if (A[start] == target){
+            return start;
+        }
+        
+        if (A[end] == target){
+            return end;
+        }
+        
+        return -1;
+    }
+    
+    private int firstGreaterThanTarget(int[] A, int target){
+        int start = 0;
+        int end = A.length - 1;
+        while (start + 1 < end){
+            int mid = (start + end)/2;
+            if (A[mid] <= target){
+                start = mid;
+            }else{
+                end = mid;
+            }
+        }
+        
+        if (A[start] > target){
+            return start;
+        }else{
+            return end;
+        }
+    }
+}
+
 public class Solution {
     /**
      * @param A: an integer sorted array
