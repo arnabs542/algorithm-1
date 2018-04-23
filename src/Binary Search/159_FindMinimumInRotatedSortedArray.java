fd88c8430ee78103756b90ade495f7ba3b4f5a8a
y.ping <yh2169@gmail.com>
2018-04-22 21:50:00 -0400

    Update 159_FindMinimumInRotatedSortedArray.java

--- src/Binary Search/159_FindMinimumInRotatedSortedArray.java
+++ src/Binary Search/159_FindMinimumInRotatedSortedArray.java
@@ -1,36 +1,27 @@
-package binarySearch;
-
-public class SearchA2DMatrix {
+public class Solution {
     /**
      * @param nums: a rotated sorted array
      * @return: the minimum number in the array
      */
     public int findMin(int[] nums) {
-        if (nums == null || nums.length == 0){
-            return -1;
-        }
-
         int start = 0;
-        int end = nums.length -1;
-
-        while(start + 1 < end){
-            int mid = start + (end - start) / 2;
-
-            if (nums[mid] < nums[end]){
-                end = mid;
-            }else if (nums[mid] > nums[end])
-            {
+        int end = nums.length - 1;
+        while (start + 1 < end){
+            int mid = (start + end)/2;
+            // 跟最後一位比, 以對付corner case like [1,2,3]
+            if (nums[mid] >= nums[nums.length - 1]){
+                // 在左半
                 start = mid;
             }else{
-                //nums[mid] == nums[start]
+                // 在右半
                 end = mid;
             }
         }
-
-        if (nums[end] < nums[start]){
+        
+        if (nums[start] < nums[end]){
+            return nums[start];
+        }else{
             return nums[end];
         }
-
-        return nums[start];
     }
 }
