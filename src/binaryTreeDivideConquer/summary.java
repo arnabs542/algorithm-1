ec65acde09879e8a6cd273d6b3f7a92896b5499b
y.ping <yh2169@gmail.com>
2018-05-06 12:30:21 -0400

    Update summary.java

--- src/binaryTreeDivideConquer/summary.java
+++ src/binaryTreeDivideConquer/summary.java
@@ -6,9 +6,18 @@
 // 596. Minimum Subtree
 // 97. Maximum Depth of Binary Tree
 // 155. Minimum Depth of Binary Tree
-// 93. Balanced Binary Tree
 
 // 453. Flatten Binary Tree to Linked List
+// => 紀錄最後一個點, 才能把right的部分接進來
+
+// 902. Kth Smallest Element in a BST
+// => inorder traverse 先找到最小的 O(h) 樹高
+// => 計算經過的k 的個樹, 找到第k小的 O(k)
+// => O(h+k)
+
+// 578. Lowest Common Ancestor III
+// => 左右都存在為LCA
+// => 回傳result 以判斷
 
 
 
