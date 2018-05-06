1e7877a75b2ae300f78efc7aa93a9a454166619b
y.ping <yh2169@gmail.com>
2018-05-06 18:10:44 -0400

    Update 95_ValidateBinarySearchTree.java

--- src/binaryTreeDivideConquer/95_ValidateBinarySearchTree.java
+++ src/binaryTreeDivideConquer/95_ValidateBinarySearchTree.java
@@ -1,23 +1,18 @@
-package binaryTreeDivideConquer;
 
-/**
- * Created by epingho on 2017/7/4.
- */
-public class ValidateBinarySearchTree {
-    public boolean isValidBST(TreeNode root) {
+public boolean isValidBST(TreeNode root) {
         return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
-
-    public boolean helper (TreeNode root, long min, long max){
+    
+    private boolean helper(TreeNode root, long min, long max){
         if (root == null){
             return true;
         }
-
+        
         if (root.val >= max || root.val <= min){
             return false;
         }
-
-        return helper(root.left, min, Math.min(root.val, max))
-                && helper(root.right, Math.max(root.val, min), max);
+        
+        boolean leftResult = helper(root.left, min, root.val);
+        boolean rightResult = helper(root.right, root.val, max);
+        return leftResult && rightResult;
     }
-}
