683d77133b25d598aafad8390039a8bea8eee3f9
y.ping <yh2169@gmail.com>
2018-05-07 13:23:50 -0400

    Update summary.java

--- src/binaryTreeDivideConquer/summary.java
+++ src/binaryTreeDivideConquer/summary.java
@@ -1,32 +1,34 @@
 // divide and counquer
 // 
-// 93. Balanced Binary Tree 
-// => 求深度
+ 93. Balanced Binary Tree 
+ => 求深度
 
-// 95. Validate Binary Search Tree
-// => recursion 傳入 lowerbound and upperbound
+ 95. Validate Binary Search Tree
+ => recursion 傳入 lowerbound and upperbound
 
-// 596. Minimum Subtree
-// 97. Maximum Depth of Binary Tree
-// 155. Minimum Depth of Binary Tree
+ 596. Minimum Subtree
+ 97. Maximum Depth of Binary Tree
+ 155. Minimum Depth of Binary Tree
 
-// 453. Flatten Binary Tree to Linked List
-// => 紀錄最後一個點, 才能把right的部分接進來
+ 453. Flatten Binary Tree to Linked List
+ => 紀錄最後一個點, 才能把right的部分接進來
 
-// 900. Closest Binary Search Tree Value
-// => 求出 lowerBound 和 upperBound。即 < target 的最大值和 >= target 的最小值, 再做比較
+ 900. Closest Binary Search Tree Value
+ => 求出 lowerBound 和 upperBound。即 < target 的最大值和 >= target 的最小值, 再做比較
 
-// 902. Kth Smallest Element in a BST
-// => inorder traverse 先找到最小的 O(h) 樹高
-// => 計算經過的k 的個樹, 找到第k小的 O(k)
-// => O(h+k)
+ 902. Kth Smallest Element in a BST
+ => inorder traverse 先找到最小的 O(h) 樹高
+ => 計算經過的k 的個樹, 找到第k小的 O(k)
+ => O(h+k)
 
-// 578. Lowest Common Ancestor III
-// => 左右都存在為LCA
-// => 回傳result 以判斷
-// => 因為要找Lowest common ancestor (A, B的第一個共同祖先) 
-// => only (LCA == null) 才算是答案
+ 578. Lowest Common Ancestor III
+ => 左右都存在為LCA
+ => 回傳result 以判斷
+ => 因為要找Lowest common ancestor (A, B的第一個共同祖先) 
+ => only (LCA == null) 才算是答案
 
+915. Inorder Predecessor in BST
+https://www.jiuzhang.com/solution/inorder-predecessor-in-bst/
 
 // DFS
 // 480. Binary Tree Paths
