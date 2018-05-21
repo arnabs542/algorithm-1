8b0bdf0e744fb881ba4e517eaec134d288d0cf8d
y.ping <yh2169@gmail.com>
2018-05-21 07:13:45 -0400

    Update Subarray.java

--- src/Subarray.java
+++ src/Subarray.java
@@ -1,1 +1,14 @@
+41. Maximum Subarray
+=> int maxSubarray, minPrefix
+=> loop through the array
+=> maxSubarray = sum - minPrefix
 
+44. Minimum Subarray
+=> 道理同上
+
+138. Subarray Sum 
+HashMap 在Array 题目中一般可以用O(n)的时间复杂度解决两种问题
+1，求两者之和为固定某数
+2，求两个index之间的所有数的和为某数
+
+=> 開個hashmap 存下所有prefix sum
