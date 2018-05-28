05f21a602a839e8a3deadbcefefd5e40c2d29fc9
y.ping <yh2169@gmail.com>
2018-05-28 16:35:09 -0400

    Update merge K sorted xxx.java

--- src/merge K sorted xxx.java
+++ src/merge K sorted xxx.java
@@ -1,4 +1,4 @@
-// === merge 2 sorted array/ list/ intervals ====
+////////  merge 2 sorted array/ list/ intervals  ////////
 6. Merge Two Sorted Arrays
 839. Merge Two Sorted Interval Lists
 165. Merge Two Sorted Lists
@@ -9,4 +9,13 @@
   }
 => 剩下的再全部放入結果
 
-2. merge K sorted array
+////////  merge K sorted array  ////////
+
+=> priorityqueue
+Queue<T> q = new PriorityQueue<T>();
+
+=> 把 K rows 的第一個element都放入PriorityQueue
+=> while(!q.isEmpty()){ 
+    q.poll()
+    q.offer(poll出那一列的下個element)
+    }
