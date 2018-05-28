e615a103f89cf38185d51b11dd0737bfd7dd86bd
y.ping <yh2169@gmail.com>
2018-05-28 16:26:44 -0400

    Update merge K sorted xxx.java

--- src/merge K sorted xxx.java
+++ src/merge K sorted xxx.java
@@ -1,1 +1,12 @@
+// === merge 2 sorted array/ list/ intervals ====
+6. Merge Two Sorted Arrays
+839. Merge Two Sorted Interval Lists
+165. Merge Two Sorted Lists
 
+=> 設定counter, i for array1, j for array2
+=> while ( i < A1.length && j < A2.length){
+    比大小, 小的先放入結果
+  }
+=> 剩下的再全部放入結果
+
+2. merge K sorted array
