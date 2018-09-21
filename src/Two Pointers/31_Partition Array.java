d4a532346da82d4e7e8722ff71bc23a7a195d6f4
y.ping <yh2169@gmail.com>
2018-09-21 15:35:28 -0400

    Update 31_Partition Array.java

--- src/Two Pointers/31_Partition Array.java
+++ src/Two Pointers/31_Partition Array.java
@@ -1,38 +1,30 @@
 public class Solution {
-	/** 
-     *@param nums: The integer array you should partition
-     *@param k: As description
-     *return: The index after partition
+    /**
+     * @param nums: The integer array you should partition
+     * @param k: An integer
+     * @return: The index after partition
      */
     public int partitionArray(int[] nums, int k) {
-        int firstK = 0;
-
-        if (nums == null || nums.length == 0){
-            return firstK;
-        }
         
-        int count = 0;
         int left = 0;
         int right = nums.length - 1;
-        
-        while (left <= right){
-            while (left <= right && nums[left] < k){
+        while(left <= right){
+            while(left <= right && nums[left] < k){
                 left ++;
-            }
-            
-            while (left <= right && nums[right] >= k){
+            } 
+            while(left <= right && nums[right] >= k){
                 right --;
-            }
+            } 
             
             if (left <= right){
-                // swap left to right
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left ++;
+                right --;
             }
         }
+        
         return right + 1;
     }
-    
-    
 }
