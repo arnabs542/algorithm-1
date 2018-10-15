2f62d4fb27d3dfef8002d7227c32e92497682521
y.ping <yh2169@gmail.com>
2018-10-15 12:21:07 -0400

    Update 453_FlattenBinaryTreeToLinkedList.java

--- src/binaryTreeDivideConquer/453_FlattenBinaryTreeToLinkedList.java
+++ src/binaryTreeDivideConquer/453_FlattenBinaryTreeToLinkedList.java
@@ -24,3 +24,40 @@ public class FlattenBinaryTreeToLinkedList {
         flatten(right);
     }
 }
+
+
+// divide and conquer
+public class Solution {
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    public void flatten(TreeNode root) {
+        helper(root);
+    }
+    
+    private TreeNode helper(TreeNode root){
+        if (root == null){
+            return null;
+        }
+        
+        TreeNode leftLast = helper(root.left);
+        TreeNode rightLast = helper(root.right);
+        
+        if(leftLast != null){
+            leftLast.right = root.right;
+            root.right = root.left;
+            root.left = null;
+        }
+        
+        if (rightLast != null){
+            return rightLast;
+        }
+        
+        if (leftLast != null){
+            return leftLast;
+        }
+        
+        return root;
+    }
+}
