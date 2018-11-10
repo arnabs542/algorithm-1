382af0ac5d3c9b12280c5ff9584fde72c1000679
y.ping <yh2169@gmail.com>
2018-11-10 10:21:10 -0500

    Create 123_Word Search.java

---
+++ src/Depth First Search/123_Word Search.java
@@ -0,0 +1,50 @@
+
+// exceed time complexity
+public class Solution {
+    /**
+     * @param board: A list of lists of character
+     * @param word: A string
+     * @return: A boolean
+     */
+    private boolean isExist = false;
+    private int[] xMove = {1, -1, 0, 0};
+    private int[] yMove = {0, 0, 1, -1};
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i ++){
+            for (int j = 0; j < board[0].length; j ++){
+                DFS(board, word, i, j, 0);
+                if (this.isExist){
+                    break;
+                }
+            }
+        }
+        
+        return isExist;
+    }
+    
+    private void DFS(char[][] board,
+                        String word,
+                        int x,
+                        int y,
+                        int wordIndex){
+        
+        if (x >= board.length || x < 0 || y >= board[0].length || y < 0){
+            return;
+        }
+        
+        if (word.charAt(wordIndex) != board[x][y]){
+            return;
+        }
+        
+        if (wordIndex == word.length() - 1){
+            this.isExist = true;
+            return;
+        }
+        
+        for (int i = 0; i < 4; i ++){
+            board[x][y] = '#';
+            DFS(board, word, x + xMove[i], y + yMove[i], wordIndex + 1);
+            board[x][y] = word.charAt(wordIndex);
+        }
+    }
+}
