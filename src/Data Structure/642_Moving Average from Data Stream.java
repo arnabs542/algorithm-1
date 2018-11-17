fa39114d204ed73361d85d9af135d47047bba716
y.ping <yh2169@gmail.com>
2018-11-17 18:03:12 -0500

    Update 642_Moving Average from Data Stream.java

--- src/Data Structure/642_Moving Average from Data Stream.java
+++ src/Data Structure/642_Moving Average from Data Stream.java
@@ -24,3 +24,32 @@ public class MovingAverage {
         return sum / q.size();
     }
 }
+
+// 滾動數組
+public class MovingAverage {
+    /*
+    * @param size: An integer
+    */
+    private double[] sum;
+    private int id;
+    private int size;
+    public MovingAverage(int size) {
+        sum = new double[size + 1];
+        this.size = size;
+    }
+
+    /*
+     * @param val: An integer
+     * @return:  
+     */
+    public double next(int val) {
+        id ++; 
+        int length = sum.length;
+        sum[id % length] = sum[(id - 1) % length] + val;
+        if (id - size >= 0){
+            return (sum[id % length] - sum[(id- size) % length]) / size;
+        } else {
+            return sum[id % length] / id;
+        }
+    }
+}
