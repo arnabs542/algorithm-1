b465bf80dc22028ccf266596cc7938b590d9a961
y.ping <yh2169@gmail.com>
2018-11-25 10:48:31 -0500

    Update 209_First Unique Character in a String.java

--- src/Data Structure/209_First Unique Character in a String.java
+++ src/Data Structure/209_First Unique Character in a String.java
@@ -74,3 +74,83 @@ public class Solution {
         return ' ';
     }
 }
+
+// Solution 4. if data is data stream - HashMap + LinkedList
+public class Solution {
+    /**
+     * @param str: str: the given string
+     * @return: char: the first unique character in a given string
+     */
+    public char firstUniqChar(String str) {
+        DataStream ds = new DataStream();
+        for (char c: str.toCharArray()){
+            ds.add(c);
+        }
+        
+        return ds.firstUniqChar();
+    }
+}
+
+// 1. maintain unique char in linked list, head => 1st unique => 2nd unique
+// 2. maintain a uniqueMap <Character, ListNode>
+// 3. maintain a duplicateSet
+// 4. when there comes a new character 
+//      => if duplicate: return;
+//      => if not duplicate: 
+//      a.) if in hashmap, remove from linkedlist, remove from hashmap, add to duplicate set
+//      b.) if not in hashmap, put(Character, ListNode), append to tail of linked List
+
+class ListNode{
+    public char c;
+    public ListNode prev;
+    public ListNode next;
+    public ListNode(char c){
+        this.c = c;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class DataStream{
+    ListNode head, tail;
+    Map<Character, ListNode> uniqueMap = new HashMap<>();
+    Set<Character> duplicateSet = new HashSet<>();
+    
+    public DataStream(){
+        head = new ListNode(' ');
+        tail = new ListNode(' ');
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public void add(char c){
+        if (duplicateSet.contains(c)){
+            return;
+        }
+        
+        if(uniqueMap.containsKey(c)){
+            ListNode curr = uniqueMap.get(c);
+            curr.prev.next = curr.next;
+            curr.next.prev = curr.prev;
+            uniqueMap.remove(c);
+            duplicateSet.add(c);
+        } else {
+            ListNode newNode = new ListNode(c);
+            uniqueMap.put(c, newNode);
+            appendToList(newNode);
+        }
+    }
+    
+    private void appendToList(ListNode curr){
+        ListNode temp = tail.prev;
+        temp.next = curr;
+        curr.next = tail;
+        tail.prev = curr;
+        curr.prev = temp;
+    }
+    
+    public char firstUniqChar(){
+        return head.next.c;
+    }
+}
+
