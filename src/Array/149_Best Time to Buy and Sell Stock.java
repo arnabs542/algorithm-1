3c1a09bdb830cabc633a3d91f261bc902b45f999
y.ping <yh2169@gmail.com>
2019-01-14 22:38:24 -0500

    Update 149_Best Time to Buy and Sell Stock.java

--- src/Array/149_Best Time to Buy and Sell Stock.java
+++ src/Array/149_Best Time to Buy and Sell Stock.java
@@ -1,20 +1,19 @@
 public class Solution {
-    /*
+    /**
      * @param prices: Given an integer array
      * @return: Maximum profit
      */
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0){
+        if (prices == null || prices.length == 0 || prices.length == 1){
             return 0;
         }
-        
-        int min = Integer.MAX_VALUE; // find the lowest point to purchase
-        int maxProfit = 0; // find the point with max profit to sell
+        int maxProfitValue = Integer.MIN_VALUE;
+        int minCost = Integer.MAX_VALUE;
         for (int i = 0; i < prices.length; i ++){
-            min = prices[i] < min? prices[i]: min;
-            maxProfit = prices[i] - min > maxProfit? prices[i] - min: maxProfit;
+            minCost = Math.min(minCost, prices[i]);
+            maxProfitValue = Math.max(maxProfitValue, prices[i] - minCost);
         }
         
-        return maxProfit;
+        return maxProfitValue;
     }
 }
