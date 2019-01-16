bcd21fc0504f53a7a0f3d71dca84ce0d6b44e133
y.ping <yh2169@gmail.com>
2019-01-15 22:54:34 -0500

    Update 547_Intersection of Two Arrays.java

--- src/Array/547_Intersection of Two Arrays.java
+++ src/Array/547_Intersection of Two Arrays.java
@@ -27,3 +27,49 @@ public class Solution {
         return result;
     }
 }
+
+/** Sol 2.) Sort & Merge O(nlogn) + O(nlogn) + O(n)
+*/
+
+public class Solution {
+    
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1); // O(nlogn)
+        Arrays.sort(nums2); // O(nlogn)
+        
+        int i = 0; 
+        int j = 0;
+        List<Integer> result = new ArrayList<Integer>();
+        while(i < nums1.length && j < nums2.length){
+            while (i < nums1.length && j < nums2.length && nums1[i] < nums2[j]){
+                i ++;
+            }
+            
+            while (i < nums1.length && j < nums2.length && nums2[j] < nums1[i]){
+                j ++;
+            }
+        
+            if (i < nums1.length && j < nums2.length && nums1[i] == nums2[j]){
+                if (result.size() == 0){
+                    result.add(nums1[i]);
+                } else {
+                    if (nums1[i] != result.get(result.size() - 1)){
+                        result.add(nums1[i]);
+                    }
+                }
+                
+                i ++;
+                j ++;
+            }
+        }
+        
+        int[] resultArray = new int[result.size()];
+        int index = 0;
+        for (int r: result){
+            resultArray[index] = r;
+            index ++;
+        }
+        return resultArray;
+        
+    }
+}
