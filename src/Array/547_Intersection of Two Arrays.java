8f17e5e04c6b9bdea804053de846ee46cfaff0c8
y.ping <yh2169@gmail.com>
2019-01-15 22:25:23 -0500

    Create 547_Intersection of Two Arrays.java

---
+++ src/Array/547_Intersection of Two Arrays.java
@@ -0,0 +1,29 @@
+/** 
+Sol 1.) HashSet
+*/
+public class Solution {
+   
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> hs = new HashSet<Integer>();
+        for (int i : nums1){
+            hs.add(i);
+        }
+        
+        Set<Integer> hsResult = new HashSet<Integer>();
+
+        for(int i : nums2){
+            if (hs.contains(i)){
+                hsResult.add(i);
+            }
+        }
+        
+        int[] result = new int[hsResult.size()];
+        int j = 0;
+        for (int i : hsResult){
+            result[j] = i;
+            j ++;
+        }
+        
+        return result;
+    }
+}
