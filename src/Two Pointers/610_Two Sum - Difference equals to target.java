f35a4822130a0c6e1d20f3160e4b16b475ad0c7a
y.ping <yh2169@gmail.com>
2019-01-20 18:37:34 -0500

    Update 610_Two Sum - Difference equals to target.java

--- src/Two Pointers/610_Two Sum - Difference equals to target.java
+++ src/Two Pointers/610_Two Sum - Difference equals to target.java
@@ -20,3 +20,30 @@ public class Solution {
         return null;
     }
 }
+
+
+public class Solution {
+    /**
+     * @param nums: an array of Integer
+     * @param target: an integer
+     * @return: [index1 + 1, index2 + 1] (index1 < index2)
+     */
+    public int[] twoSum7(int[] nums, int target) {
+        Map<Integer, Integer> hm = new HashMap<>();
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i ++){
+            int diff1 = nums[i] + target;
+            int diff2 = nums[i] - target;
+            if (hm.containsKey(nums[i])){
+                result[0] = hm.get(nums[i]) + 1;
+                result[1] = i + 1;
+            }  else {
+                hm.put(diff1, i);
+                hm.put(diff2, i);
+            }
+        }
+        
+        return result;
+    }
+}
+
