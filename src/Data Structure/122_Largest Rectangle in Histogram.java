d6675756eb21e0bcd2509972873e9c7214bd47b7
y.ping <yh2169@gmail.com>
2019-01-27 12:19:07 -0500

    Create 122_Largest Rectangle in Histogram.java

---
+++ src/Data Structure/122_Largest Rectangle in Histogram.java
@@ -0,0 +1,31 @@
+public class Solution {
+    /**
+     * @param height: A list of integer
+     * @return: The area of largest rectangle in the histogram
+     */
+    public int largestRectangleArea(int[] height) {
+        if (height == null || height.length == 0){
+            return 0;
+        }
+        
+        int max = 0;
+        Stack<Integer> s = new Stack<Integer>();
+        for (int i = 0; i <= height.length; i ++){
+            int curr;
+            if (i == height.length){
+                curr = -1;
+            } else {
+                curr = height[i];
+            }
+            
+            while(!s.isEmpty() && curr <= height[s.peek()]){
+                int h = height[s.pop()];
+                int w = s.isEmpty()? i : i - 1 - s.peek();
+                max = Math.max(max, h * w);
+            }
+            s.push(i);
+        }
+        
+        return max;
+    }
+}
