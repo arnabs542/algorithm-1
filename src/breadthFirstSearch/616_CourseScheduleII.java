1cd98eda20dcb106509b444a77e01028087261d7
y.ping <yh2169@gmail.com>
2019-02-04 17:44:10 -0500

    Update 616_CourseScheduleII.java

--- src/breadthFirstSearch/616_CourseScheduleII.java
+++ src/breadthFirstSearch/616_CourseScheduleII.java
@@ -1,58 +1,59 @@
-package breadthFirstSearch;
-
-import java.util.*;
-
-/**
- * Created by epingho on 2017/7/5.
- */
-
-// have to review.
-// my code doesn't work
-public class CourseScheduleII {
+public class Solution {
+    /*
+     * @param numCourses: a total of n courses
+     * @param prerequisites: a list of prerequisite pairs
+     * @return: the course order
+     */
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        if (numCourses == 0 || prerequisites == null || prerequisites.length == 0){
-            return new int[0];
+        HashMap<Integer, Set<Integer>> nextCourse = new HashMap<>();
+        HashMap<Integer, Integer> preReq = new HashMap<>();
+        
+        for (int i = 0; i < numCourses; i ++){
+            nextCourse.put(i, new HashSet<Integer>());
+            preReq.put(i, 0);
         }
-
-        int[] result = new int[numCourses];
-        int[] degree = new int[numCourses];
-        Queue<Integer> thisQ = new LinkedList<>();
-        Map<Integer, Set<Integer>> mapping = new HashMap<Integer, Set<Integer>>();
-
+        
         for (int i = 0; i < prerequisites.length; i ++){
-            degree[prerequisites[i][0]] ++;
-            if (!mapping.containsKey(prerequisites[i][1])){
-                mapping.put(prerequisites[i][1], new HashSet<Integer>());
-            }
-            mapping.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            nextCourse.get(prerequisites[i][1]).add(prerequisites[i][0]);
         }
-
-        for (int i = 0; i < degree.length; i++){
-            if (degree[i] == 0){
-                thisQ.offer(i);
+        
+        /* use nieghbors to get prerequisites count 
+         * to avoid duplicate entries in prerequisites[][]
+         */
+        for (int i: nextCourse.keySet()){
+            for (int j : nextCourse.get(i)){
+                preReq.put(j, preReq.get(j) + 1);
             }
         }
-
-        int count = 0;
-        while (!thisQ.isEmpty()){
-            int thisNode = thisQ.poll();
-            result[count] = thisNode;
-            count++;
-            if (mapping.get(thisNode) != null){
-                for (int prerequisite : mapping.get(thisNode)){
-                    degree[prerequisite] --;
-                    if (degree[prerequisite] == 0){
-                        thisQ.offer(prerequisite);
-                    }
+        
+        Queue<Integer> q = new LinkedList<Integer>();
+        for (int i : preReq.keySet()){
+           if (preReq.get(i) == 0){
+               q.offer(i);
+           } 
+        }
+        
+        List<Integer> result = new ArrayList<Integer>();
+        while(!q.isEmpty()){
+            int course = q.poll();
+            result.add(course);
+            for (int n : nextCourse.get(course)){
+                preReq.put(n, preReq.get(n) - 1);
+                if (preReq.get(n) == 0){
+                    q.offer(n);
                 }
             }
         }
-
-        if (count == numCourses){
-            return result;
+        
+        if (result.size() != numCourses){
+            return new int[0];
         }
-
-        return new int[0];
+        
+        int[] resultArray= new int[numCourses];
+        for (int i = 0; i < result.size(); i ++){
+            resultArray[i] = result.get(i);
+        }
+        
+        return resultArray;
     }
-
 }
