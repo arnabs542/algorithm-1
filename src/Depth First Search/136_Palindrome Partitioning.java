c9a956d509d623fa364cd51a82c06f1be497af04
y.ping <yh2169@gmail.com>
2019-02-09 16:18:00 -0500

    Update 136_Palindrome Partitioning.java

--- src/Depth First Search/136_Palindrome Partitioning.java
+++ src/Depth First Search/136_Palindrome Partitioning.java
@@ -1,42 +1,38 @@
 public class Solution {
-    /**
+    /*
      * @param s: A string
      * @return: A list of lists of string
      */
     public List<List<String>> partition(String s) {
         List<List<String>> result = new ArrayList<>();
-        
-        if (s == null || s.length() == 0){
-            return result;
-        }
-        
-        List<String> temp = new ArrayList<String>();
-        dfs(s, 0, temp, result);
+        boolean[][] memo = new boolean[s.length()][s.length()];
+        dfs(s, 0, new ArrayList<String>(), result, memo);
         return result;
     }
     
-    private void dfs(String s
-            , int startIndex
-            , List<String> temp
-            , List<List<String>> result){
-                
-        if (startIndex == s.length()){
-            result.add(new ArrayList<String>((temp)));
+    private void dfs(String s,
+                    int startIndex,
+                    List<String> subset,
+                    List<List<String>> result,
+                    boolean[][] memo){
+        
+        if (startIndex > s.length() - 1){
+            result.add(new ArrayList<String>(subset));
             return;
         }
         
         for (int i = startIndex; i < s.length(); i ++){
             String x = s.substring(startIndex, i + 1);
-            if (!isPalindrome(x)){
-                continue;
+            if (memo[startIndex][i] || isPalidrome(x)){
+                memo[startIndex][i] = true;
+                subset.add(x);
+                dfs(s, i + 1, subset, result, memo);
+                subset.remove(subset.size() - 1);
             }
-            temp.add(x);
-            dfs(s, i + 1, temp, result);
-            temp.remove(temp.size() - 1);
         }
     }
     
-    private boolean isPalindrome(String s){
+    private boolean isPalidrome(String s){
         for (int i = 0, j = s.length() - 1; i < j; i ++, j --){
             if (s.charAt(i) != s.charAt(j)){
                 return false;
