6c45fb7bb40e380222df13999c999d3976ab130b
y.ping <yh2169@gmail.com>
2019-02-17 16:21:56 -0500

    Update 175_Invert Binary Tree.java

--- src/binaryTreeDivideConquer/175_Invert Binary Tree.java
+++ src/binaryTreeDivideConquer/175_Invert Binary Tree.java
@@ -35,4 +35,40 @@ public class Solution {
 }
 
 /** Sol 2.) iteration
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
 
+public class Solution {
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    public void invertBinaryTree(TreeNode root) {
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        s.push(root);
+        while (!s.isEmpty()){
+            TreeNode n = s.pop();
+            
+            TreeNode temp = n.left;
+            n.left = n.right;
+            n.right = temp;
+            
+            if (n.left != null){
+                s.push(n.left);
+            }
+            
+            if (n.right != null){
+                s.push(n.right);
+            }
+        }
+    }
+}
