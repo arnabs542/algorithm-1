56b42dcdc6070efb1189a4f5c1e5af56eececc7e
y.ping <yh2169@gmail.com>
2019-02-18 16:37:58 -0500

    Update 68_Binary Tree Postorder Traversal.java

--- src/binaryTreeDivideConquer/68_Binary Tree Postorder Traversal.java
+++ src/binaryTreeDivideConquer/68_Binary Tree Postorder Traversal.java
@@ -1,3 +1,5 @@
+// Sol 1.) iteration
+
 /**
  * Definition of TreeNode:
  * public class TreeNode {
@@ -50,5 +52,45 @@ public class Solution {
         }
         
         return result;
+    }
+}
+
+// Sol 2.) recursion
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Postorder in ArrayList which contains node values.
+     */
+    // left, right, root
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        helper(root, result);
+        
+        return result;
+    }
+    
+    private void helper(TreeNode root, List<Integer> result){
+        if (root == null){
+            return;
+        }
+        
+        helper(root.left, result);
+        helper(root.right, result);
+        result.add(root.val);
+        return;
+
     }
 }
