b04c996155a8c5e1a8c37da444aa34b1a53a7dba
y.ping <yh2169@gmail.com>
2019-02-18 16:32:31 -0500

    Create 68_Binary Tree Postorder Traversal.java

---
+++ src/binaryTreeDivideConquer/68_Binary Tree Postorder Traversal.java
@@ -0,0 +1,54 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Postorder in ArrayList which contains node values.
+     */
+    public enum Operation{
+      PROCESS,
+      ADDTORESULT
+    }
+    
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        Stack<Operation> op = new Stack<Operation>();
+
+        s.push(root);
+        op.push(Operation.PROCESS);
+        
+        while(!s.isEmpty()){
+            TreeNode n = s.pop();
+            Operation o = op.pop();
+            if (n == null){
+                continue;
+            }
+            
+            if (o == Operation.ADDTORESULT){
+                result.add(n.val);
+            } else {
+                s.push(n);
+                op.push(Operation.ADDTORESULT);
+                
+                s.push(n.right);
+                op.push(Operation.PROCESS);
+                
+                s.push(n.left);
+                op.push(Operation.PROCESS);
+            }
+        }
+        
+        return result;
+    }
+}
