a46092405b9698a86c882b732ef9b4c96efbcaf7
y.ping <yh2169@gmail.com>
2019-02-23 15:30:23 -0500

    Update 902_Kth Smallest Element in a BST.java

--- src/binaryTreeDivideConquer/902_Kth Smallest Element in a BST.java
+++ src/binaryTreeDivideConquer/902_Kth Smallest Element in a BST.java
@@ -41,3 +41,58 @@ public class Solution {
         
     }
 }
+
+/**
+
+Follow up: 二叉树经常被修改
+
+在 TreeNode 中增加一个 counter，代表整个树的节点个数
+也可以用一个 HashMap<TreeNode, Integer> 来存储某个节点为代表的子树的节点个数
+在增删查改的过程中记录不断更新受影响节点的 counter
+在 kthSmallest 的实现中用类似 Quick Select 的算法去找到 kth smallest element
+时间复杂度为 O(h)，h 为树的高度。
+Strong Hire: 能够答出 Follow Up 的算法，并写出kthSmallest核心代码（不需要写增删查改，45分钟写
+不完的）， bug free or minor bug，不需要提示
+Hire / Weak Hire : 能够答出 Follow up 的算法，大致写出 kthSmallest 核心代码，存在一定bug，或者需
+要提示
+ */
+
+public class Solution {
+    /**
+     * @param root: the given BST
+     * @param k: the given k
+     * @return: the kth smallest element in BST
+     */
+     
+    // L, root, R
+    HashMap<TreeNode, Integer> hm = new HashMap<>();
+    int count = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        getSmallerNodeCount(root);
+        return getkth(root, k);
+    }
+    
+    private void getSmallerNodeCount(TreeNode root){
+        if (root == null){
+            return;
+        }
+        
+        getSmallerNodeCount(root.left);
+        count ++;
+        hm.put(root, count);
+        getSmallerNodeCount(root.right);
+    }
+    
+    private int getkth(TreeNode root, int k){
+        int count = hm.get(root);
+        if (count == k){
+            return root.val;
+            
+        } else if (count > k){
+            return getkth(root.left, k);
+            
+        } else {
+            return getkth(root.right, k);
+        }
+    }
+}
