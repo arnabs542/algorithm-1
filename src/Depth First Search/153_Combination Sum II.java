fe1ed676c239d3b1f8f07c10fc5f4c9d6ed07876
y.ping <yh2169@gmail.com>
2019-03-02 22:26:00 -0500

    Update 153_Combination Sum II.java

--- src/Depth First Search/153_Combination Sum II.java
+++ src/Depth First Search/153_Combination Sum II.java
@@ -5,41 +5,39 @@ public class Solution {
      * @return: All the combinations that sum to target
      */
     public List<List<Integer>> combinationSum2(int[] num, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (num == null){
-            return result;
-        }
-        
+        // 1. sort 
+        // 2. DFS(int startIndex)
         Arrays.sort(num);
-        dfs(num, target, 0, new ArrayList<Integer>(), result);
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        DFS(0, num, subset, result, target);
+        
         return result;
     }
     
-    //1, 1, 2, 5, 6, 7, 10
-    
-    private void dfs(int[] nums
-                , int target
-                , int startIndex
-                , List<Integer> subset
-                , List<List<Integer>> result
-        ){
-            if (target == 0){
-                result.add(new ArrayList<Integer>(subset));
-                System.out.println("resut add: " + subset);
+    private void DFS(int startIndex, 
+                    int[] num,
+                    List<Integer> subset,
+                    List<List<Integer>> result, 
+                    int target){
+                         
+        if (target == 0){
+            result.add(new ArrayList<Integer>(subset));
+            return;
+        }
+        
+        for (int i = startIndex; i < num.length; i ++){
+            if (num[i] > target){
                 return;
             }
             
-            for (int i = startIndex; i < nums.length; i ++){
-                if (i != 0 && i != startIndex && nums[i] == nums[i - 1]){
-                    continue;
-                }
-                if (nums[i] > target){
-                    break;
-                }
-                subset.add(nums[i]);
-                dfs(nums, target - nums[i], i + 1, subset, result);
-                subset.remove(subset.size() - 1);
+            if (i > startIndex && num[i] == num[i - 1]){
+                continue;
             }
-        
+            
+            subset.add(num[i]);
+            DFS(i + 1, num, subset, result, target - num[i]);
+            subset.remove(subset.size() - 1);
+        }
     }
 }
