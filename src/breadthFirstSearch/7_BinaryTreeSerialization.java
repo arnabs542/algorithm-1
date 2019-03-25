f919bb3e92ed9caa6fcef3d8e781ce483f646af0
y.ping <yh2169@gmail.com>
2019-03-24 22:25:24 -0400

    Update 7_BinaryTreeSerialization.java

--- src/breadthFirstSearch/7_BinaryTreeSerialization.java
+++ src/breadthFirstSearch/7_BinaryTreeSerialization.java
@@ -1,82 +1,83 @@
-package breadthFirstSearch;
-
 /**
- * Created by epingho on 2017/7/5.
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
  */
-public class BinaryTreeSerialization {
+
+
+public class Solution {
     /**
-     * This method will be invoked first, you should design your own algorithm
+     * This method will be invoked first, you should design your own algorithm 
      * to serialize a binary tree which denote by a root node to a string which
      * can be easily deserialized by your own "deserialize" method later.
      */
     public String serialize(TreeNode root) {
-        if (root == null){
-            return "{}";
-        }
-
-        ArrayList<TreeNode> nodeList = new ArrayList<TreeNode>();
-        nodeList.add(root);
-        for (int i = 0; i < nodeList.size(); i++){
-            TreeNode thisNode = nodeList.get(i);
-            if (thisNode == null){
-                continue;
-            }
-            nodeList.add(thisNode.left);
-            nodeList.add(thisNode.right);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("{");
-        sb.append(nodeList.get(0).val);
-        for (int i = 1; i < nodeList.size(); i++){
-            if (nodeList.get(i) == null){
-                sb.append(",#");
-            }else{
-                sb.append(",");
-                sb.append(nodeList.get(i).val);
+        StringBuilder s = new StringBuilder();
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for (int i = 0; i < size; i ++){
+                TreeNode n = q.poll();
+                s.append(",");
+                if (n == null){
+                    s.append("#");
+                } else {
+                    s.append(Integer.toString(n.val));
+                    q.offer(n.left);
+                    q.offer(n.right);
+                }
             }
         }
-        sb.append("}");
-
-        return sb.toString();
+        
+        return "{" + s.toString().substring(1, s.length()) + "}";
     }
 
     /**
      * This method will be invoked second, the argument data is what exactly
      * you serialized at method "serialize", that means the data is not given by
      * system, it's given by your own serialize method. So the format of data is
-     * designed by yourself, and deserialize it here as you serialize it in
+     * designed by yourself, and deserialize it here as you serialize it in 
      * "serialize" method.
      */
     public TreeNode deserialize(String data) {
         data = data.replace("{", "").replace("}", "");
-        if (data.equals("")){
+        if (data == "" || data.equals("#")){
             return null;
         }
+        String[] c = data.split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(c[0]));
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        
+        boolean isLeftNode = true;
+        for (int i = 1; i < c.length; i ++){
+            if (!c[i].equals("#")){
+                TreeNode newNode = new TreeNode(Integer.parseInt(c[i])); 
 
-        String[] dataArray = data.split(",");
-        TreeNode root = new TreeNode(Integer.parseInt(dataArray[0]));
-        ArrayList<TreeNode> tempArray = new ArrayList<TreeNode>();
-        tempArray.add(root);
-
-        int index = 0;
-        boolean isLeftChild = true;
-        for (int i = 1; i < dataArray.length; i++) {
-            if (!dataArray[i].equals("#")) {
-                TreeNode node = new TreeNode(Integer.parseInt(dataArray[i]));
-                if (isLeftChild) {
-                    tempArray.get(index).left = node;
+                if (isLeftNode){
+                    q.peek().left = newNode;
                 } else {
-                    tempArray.get(index).right = node;
+                    q.peek().right = newNode;
                 }
-                tempArray.add(node);
+                
+                q.offer(newNode);
+            } 
+            
+            // 加完右子樹, 就poll root
+            if (!isLeftNode){
+                q.poll();
             }
-            if (!isLeftChild) {
-                index++;
-            }
-            isLeftChild = !isLeftChild;
+            isLeftNode = !isLeftNode;
         }
+        
         return root;
-
     }
 }
