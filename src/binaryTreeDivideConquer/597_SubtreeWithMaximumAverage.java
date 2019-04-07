eedc689ad48ba9f43dea3f5ad8c0d505488f58ab
y.ping <yh2169@gmail.com>
2019-04-07 10:49:19 -0400

    Update 597_SubtreeWithMaximumAverage.java

--- src/binaryTreeDivideConquer/597_SubtreeWithMaximumAverage.java
+++ src/binaryTreeDivideConquer/597_SubtreeWithMaximumAverage.java
@@ -1,10 +1,20 @@
-package binaryTreeDivideConquer;
-
 /**
- * Created by epingho on 2017/6/27.
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
  */
-public class 597_SubtreeWithMaximumAverage {
-
+public class Solution {
+    /**
+     * @param root the root of binary tree
+     * @return the root of the maximum average of subtree
+     */
+     
     private class Result {
         public int result_sum;
         public int result_size;
@@ -13,37 +23,37 @@ public class 597_SubtreeWithMaximumAverage {
             this.result_size = size;
         }
     }
-
+    
     private TreeNode subtree = null;
     private Result subtreeResult = null;
-
+    
     public TreeNode findSubtree2(TreeNode root) {
         helper(root);
         return subtree;
     }
-
+    
     public Result helper(TreeNode root){
         if (root == null){
             return new Result(0, 0);
         }
-
+        
         Result resultL = helper(root.left);
         Result resultR = helper(root.right);
         Result resultRoot = new Result(
-                resultL.result_sum + resultR.result_sum + root.val,
-                resultL.result_size + resultR.result_size + 1
-        );
-
-        if (subtree == null ||
-                // subtreeResult.result_sum / subtreeResult.result_size <
-                // resultRoot.result_sum / resultRoot.result_sum
-                subtreeResult.result_sum * resultRoot.result_size <
-                        resultRoot.result_sum * subtreeResult.result_size
-                ){
+            resultL.result_sum + resultR.result_sum + root.val,
+            resultL.result_size + resultR.result_size + 1
+            );
+        
+        if (subtree == null || 
+            // subtreeResult.result_sum / subtreeResult.result_size < 
+            // resultRoot.result_sum / resultRoot.result_sum
+            subtreeResult.result_sum * resultRoot.result_size < 
+            resultRoot.result_sum * subtreeResult.result_size
+            ){
             subtree = root;
             subtreeResult = resultRoot;
         }
-
+        
         return resultRoot;
     }
 }
