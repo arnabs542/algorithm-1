8c1e421d17da088f77518ea604ea787ae1c816b7
y.ping <yh2169@gmail.com>
2019-04-07 02:26:55 -0400

    Update 613_High Five.java

--- src/Data Structure/613_High Five.java
+++ src/Data Structure/613_High Five.java
@@ -1,3 +1,6 @@
+// use Priority Queue     
+// O(nlog5)
+
 /**
  * Definition for a Record
  * class Record {
@@ -14,6 +17,7 @@ public class Solution {
      * @return find the average of 5 highest scores for each person
      * Map<Integer, Double> (student_id, average_score)
      */
+    // O(nlog5)
     public Map<Integer, Double> highFive(Record[] results) {
         Map<Integer, PriorityQueue<Integer>> resultMapping = new HashMap<Integer, PriorityQueue<Integer>>();
         Map<Integer, Double> returnMapping = new HashMap<Integer, Double>();
@@ -48,5 +52,56 @@ public class Solution {
         }
         
         return returnMapping;
+    }
+}
+
+// 不用priority queue
+// O(nlogn)
+/**
+ * Definition for a Record
+ * class Record {
+ *     public int id, score;
+ *     public Record(int id, int score){
+ *         this.id = id;
+ *         this.score = score;
+ *     }
+ * }
+ */
+public class Solution {
+    // 不用priority queue
+    // O(nlogn)
+    private Comparator compareRecord = new Comparator<Record>(){
+        public int compare(Record r1, Record r2){
+            if (r1.id == r2.id){
+                // 分數從大排到小
+                return r2.score - r1.score;
+            }
+            
+            return r1.id - r2.id;
+        }
+    };
+    public Map<Integer, Double> highFive(Record[] results) {
+        // 把所有record排序 
+        List<Record> recordList = new ArrayList<Record>();
+        for (int i = 0; i < results.length; i ++){
+            recordList.add(results[i]);
+        }
+        
+        Collections.sort(recordList, compareRecord);
+        
+        HashMap<Integer, Double> ans = new HashMap<>();
+        for (int i = 0; i < recordList.size(); i ++){
+            int id = recordList.get(i).id;
+            if (!ans.containsKey(id)){
+                int sum = 0;
+                for (int j = i; j < i + 5; j ++){
+                    sum = sum + recordList.get(j).score;
+                }
+                
+                ans.put(id, sum / 5.0);
+            }
+        }
+        
+        return ans;
     }
 }
