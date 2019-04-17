1fc86d5dc12480852f800eb0a12df4f494a6729e
y.ping <yh2169@gmail.com>
2019-04-16 22:47:52 -0400

    Update 104_Merge k Sorted Lists.java

--- src/Data Structure/104_Merge k Sorted Lists.java
+++ src/Data Structure/104_Merge k Sorted Lists.java
@@ -53,6 +53,73 @@ public class Solution {
 }
 
 // Solution 2) 兩倆合併, merge 2 sorted list: O(nlogk) 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0){
+            return null;
+        }
+        // divide & counquer
+        // 把lists k 分到最小 再來合併
+        return mergeHelper(lists, 0, lists.length - 1);
+    }
+    
+    private ListNode mergeHelper(ListNode[] lists, int start, int end){
+        if (start == end){
+            return lists[start];
+        }
+        
+        int mid = start + (end - start)/2;
+        ListNode left = mergeHelper(lists, start, mid);
+        ListNode right = mergeHelper(lists, mid + 1, end);
+        return merge(left, right);
+    }
+    
+    private ListNode merge(ListNode left, ListNode right){
+        if (left == null){
+            return right;
+        }
+        
+        if (right == null){
+            return left;
+        }
+        
+        ListNode result = new ListNode(0);
+        ListNode curr = result;
+        while (left != null && right != null){
+            if (left.val < right.val){
+                curr.next = left;
+                left = left.next;
+            } else {
+                curr.next = right;
+                right = right.next;
+            }
+            
+            curr = curr.next;
+        }
+        
+        while (left != null){
+            curr.next = left;
+            curr = curr.next;
+            left = left.next;
+        }
+        
+        while (right != null){
+            curr.next = right;
+            curr = curr.next;
+            right = right.next;
+        }
+        
+        return result.next;
+    }
+}
 
 // Solution 3) Merge sort 歸併排序 O(nlogk)
 
