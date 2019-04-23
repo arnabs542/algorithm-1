021d5d29a9020c18f177779d52b250ae8d2e76bc
y.ping <yh2169@gmail.com>
2019-04-22 23:54:07 -0400

    Update LC105_Construct Binary Tree from Preorder and Inorder Traversal.java

--- src/binaryTreeDivideConquer/LC105_Construct Binary Tree from Preorder and Inorder Traversal.java
+++ src/binaryTreeDivideConquer/LC105_Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -1,6 +1,6 @@
 // preorder: Root, left, right
 // inorder: left, Root, right
-// recursive
+// recursive O(N)
 
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
@@ -33,5 +33,52 @@ class Solution {
         }
         
         return -1;
+    }
+}
+
+
+// iterative
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        TreeNode currRoot = null;
+        int preIndex = 0;
+        int inIndex = 0;
+        TreeNode root = null;
+        while (preIndex < preorder.length && inIndex < inorder.length){
+            do {
+                TreeNode n = new TreeNode(preorder[preIndex]);
+                if (root == null){
+                    root = n;
+                }
+                s.push(n);
+                if (s.isEmpty()){
+                    currRoot.right = n;
+                } else {
+                    s.peek().left = n;
+                }
+                if (preIndex == preorder.length - 1){
+                    break;
+                }
+                preIndex ++;  
+            } while (preIndex < preorder.length && preorder[preIndex] == inorder[inIndex]);
+            
+            while (!s.isEmpty() && inIndex < inorder.length && s.peek().val == inorder[inIndex]){
+                currRoot = s.pop();         
+                inIndex ++;
+            }
+            
+        }
+        
+        return root;
     }
 }
