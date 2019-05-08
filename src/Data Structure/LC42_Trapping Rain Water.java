07977ffa8ac4faf4ae714a6780caa69683b9c142
y.ping <yh2169@gmail.com>
2019-05-08 18:19:43 -0400

    Update LC42_Trapping Rain Water.java

--- src/Data Structure/LC42_Trapping Rain Water.java
+++ src/Data Structure/LC42_Trapping Rain Water.java
@@ -1,6 +1,40 @@
 // Time: O(N)
 class Solution {
     public int trap(int[] height) {
+        if (height == null || height.length == 0){
+            return 0;
+        }
+        int l = 0;
+        int r = height.length - 1;
+        int l_height = height[l];
+        int r_height = height[r];
+        int water = 0;
+        while (l < r){
+            if (height[l] < height[r]){
+                l ++;
+                if (height[l] < l_height){ // water
+                    water = water + l_height - height[l];
+                } else {
+                    l_height = height[l];
+                }
+            } else {
+                r --;
+                if (height[r] < r_height){ // water
+                    water = water + r_height - height[r];
+                } else {
+                    r_height = height[r];
+                }
+            }
+        }
+        
+        return water;
+            
+    }
+}
+
+// Time: O(N)
+class Solution {
+    public int trap(int[] height) {
         Stack<Integer> stack = new Stack<Integer>();
         int totalWater = 0;
         for (int i = 0; i < height.length; i ++){
