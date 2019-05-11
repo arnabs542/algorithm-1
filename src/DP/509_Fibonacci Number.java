04601a1efc6de16c0b55dd6952c41ccaaa212fb6
y.ping <yh2169@gmail.com>
2019-05-11 00:05:33 -0400

    Create 509_Fibonacci Number.java

---
+++ src/DP/509_Fibonacci Number.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int fib(int N) {
+        int[] memo = new int[N + 1];
+        return helper(N, memo);
+    }
+    
+    private int helper(int N, int[] memo){
+        if (N == 0){
+            return 0;
+        }
+        if (N == 1){
+            return 1;
+        }
+        if (memo[N] > 0){
+            return memo[N];
+        }
+        int ans = helper(N - 1, memo) + helper(N - 2, memo);
+        memo[N] = ans;
+        return ans;
+    }
+}
+
+
+class Solution {
+    public int fib(int N) {
+        if (N == 0){
+            return 0;
+        }
+        if (N == 1){
+            return 1;
+        }
+        
+        int[] ans = new int[N + 1];
+        ans[0] = 0;
+        ans[1] = 1;
+        for (int i = 2; i <= N; i ++){
+            ans[i] = ans[i - 1] + ans[i - 2];
+        }
+        
+        return ans[N];
+    }
+}
