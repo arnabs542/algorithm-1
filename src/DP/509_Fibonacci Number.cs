f5d70ba3a4547fbcf6fb31e44e8dea50c83b6988
y.ping <yh2169@gmail.com>
2020-07-09 11:06:35 -0400

    Update 509_Fibonacci Number.cs

--- src/DP/509_Fibonacci Number.cs
+++ src/DP/509_Fibonacci Number.cs
@@ -1,3 +1,11 @@
+Find the Nth number in Fibonacci sequence.
+
+The first two numbers are 0 and 1.
+The i th number is the sum of i-1 th number and i-2 th number.
+The first ten numbers in Fibonacci sequence is:
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ...
+
 // 0, 1, 1, 2, 3, 5, 8, 13, 21 …. 
 
 // num = 1 => 0
